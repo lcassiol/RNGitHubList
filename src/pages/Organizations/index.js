4c74e289ca7efa756edf4f2ae68556cef3401e6a
Cassio C <lccassiol@gmail.com>
2019-04-27 23:56:58 -0300

    Implements screen organization and create organizationItem

--- src/pages/Organizations/index.js
+++ src/pages/Organizations/index.js
@@ -1,19 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import { View } from 'react-native';
+import { View, AsyncStorage, ActivityIndicator, Text, FlatList } from 'react-native';
+
+import api from '~/services/api';
 
 import PropTypes from 'prop-types';
+
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 import Header from '~/components/Header';
 
-// import { Container } from './styles';
-
-const Organizations = () => (
-  <View>
-    <Header title="Organizações" />
-  </View>
-);
+import OrganizationItem from './OrganizationItem';
+import styles from './styles';
 
 const TabIcon = ({ tintColor }) => <Icon name="building" size={20} color={tintColor} />;
 
@@ -21,8 +19,57 @@ TabIcon.propTypes = {
   tintColor: PropTypes.string.isRequired,
 };
 
-Organizations.navigationOptions = {
-  tabBarIcon: TabIcon,
-};
+export default class Organizations extends Component {
+  static navigationOptions = {
+    tabBarIcon: TabIcon,
+  };
 
-export default Organizations;
+  state = {
+    data: [],
+    loading: true,
+    refreshing: false,
+  };
+
+  async componentDidMount() {
+    this.loadOrganizations();
+  }
+
+  loadOrganizations = async () => {
+    this.setState({ refreshing: true });
+
+    const username = await AsyncStorage.getItem('@RNGithubList:username');
+    const { data } = await api.get(`users/${username}/orgs`);
+
+    this.setState({ data, loading: false, refreshing: false });
+  };
+
+  renderListItem = ({ item }) => <OrganizationItem organization={item} />;
+
+  renderList = () => {
+    const { data, refreshing } = this.state;
+
+    return (
+      <FlatList
+        data={data}
+        keyExtractor={item => String(item.id)}
+        renderItem={this.renderListItem}
+        onRefresh={this.loadOrganizations}
+        refreshing={refreshing}
+        numColumns={2}
+        columnWrapperStyle={styles.columnWrapper}
+      />
+    );
+  };
+
+  render() {
+    const { loading } = this.state;
+
+    return (
+      <View style={styles.container}>
+        <Header title="Organizações" />
+
+        {loading ? <ActivityIndicator style={styles.loading} /> : this.renderList()}
+      </View>
+    );
+  }
+}
